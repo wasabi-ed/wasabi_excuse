020d2061acb7124d0b13aad81ac5656973e59d4d
wasabi-ed <emendieta90@gmail.com>
2020-09-25 19:12:10 +0000

    Excuse generator commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,30 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", function() {
+    document.querySelector("#the-excuse").innerHTML = generateexcuse();
+  });
+  //   console.log("Hello Rigo from the console!");
+};
+
+let generateexcuse = function() {
+  let who = ["My dog", "The baby", "Nosferatu", "Your momma"];
+  let action = ["lost", "ruined", "took a poo on", "nuked"];
+  let what = ["my laptop", "my phone", "my keys", "my sanity"];
+  let when = ["yesterday", "just now", "before class", "an hour ago"];
+
+  let whoIndex = Math.floor(Math.random() * 4);
+  let actionIndex = Math.floor(Math.random() * 4);
+  let whatIndex = Math.floor(Math.random() * 4);
+  let whenIndex = Math.floor(Math.random() * 4);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
